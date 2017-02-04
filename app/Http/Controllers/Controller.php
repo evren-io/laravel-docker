454894cc5fd57ae30ef7c213554bbcb8dfef6022
Paul Aan <weburnittv@gmail.com>
2017-02-04 18:21:57 +0700

    First Sample

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    public function home()
-    {
-        return view('home.index', ['name' => 'Paul']);
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- resources/views/home/index.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h1>Hello, <?php echo $name; ?></h1>
-</body>
-</html>
